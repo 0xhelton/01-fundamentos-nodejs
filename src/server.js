7a083dfc0679e144a952d63dbcd794d28ad6c50d
0xhelton <0xhelton@gmail.com>
2024-05-11 19:34:24 -0300

    :sparkles: feat: method GET, method POST

---
+++ src/server.js
@@ -0,0 +1,43 @@
+import http, { request } from "node:http";
+
+/*
+================
+      HTTP
+================
+- Método HTTP
+- Url
+
+================================
+  GET, POST, PUT, PATCH, DELETE
+================================
+- GET => Buscar um recurso no Back-end
+- POST => Criar um recurso no Back-end
+- PUT => Atualizar um recurso no Back-end
+- PATCH => Atualizar uma informação especifica de um recurso no Back-end
+- DELETE => Deletar um recurso no Back-end
+*/
+
+// JSON - JavaScript Object Notation
+
+const users = [];
+
+const server = http.createServer((resquest, response) => {
+  const { method, url } = resquest;
+
+  if (method === "GET" && url === "/users") {
+    .setHeader('Content-type')
+    return response.end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "Helton Muniz",
+      email: "0xhelton@gmail.com",
+    });
+  }
+
+  return response.end("Hello World");
+});
+
+server.listen(3333);
