2d423b598d6dc348ff1d70480033c979cc14706e
0xhelton <0xhelton@gmail.com>
2024-05-12 18:16:45 -0300

    :sparkles: feat: add Regex

--- src/server.js
+++ src/server.js
@@ -1,54 +1,20 @@
 import http from "node:http";
-import { randomUUID } from "node:crypto";
 import { json } from "./middleware/json.js";
-import { Database } from "./middleware/database.js";
-
-/*
-================
-      HTTP
-================
-- Método HTTP
-- Url
-
-================================
-  GET, POST, PUT, PATCH, DELETE
-================================
-- GET => Buscar um recurso no Back-end
-- POST => Criar um recurso no Back-end
-- PUT => Atualizar um recurso no Back-end
-- PATCH => Atualizar uma informação especifica de um recurso no Back-end
-- DELETE => Deletar um recurso no Back-end
-*/
-
-// JSON - JavaScript Object Notation
-
-// Headers - (Request, Response) => Metadados
-
-const database = new Database();
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
 
   await json(request, response);
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select("users");
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url;
+  });
 
-    return response.end(JSON.stringify(users));
-  }
+  console.log(route);
 
-  if (method === "POST" && url === "/users") {
-    const { name, email } = request.body;
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    };
-
-    database.insert("users", user);
-
-    return response.writeHead(201).end();
+  if (route) {
+    return route.handler(request, response);
   }
 
   return response.writeHead(404).end();
